717c4b0dacdcbadcfbb844454a38ae644433da9e
Divijadiwan <79202293+Divijadiwan@users.noreply.github.com>
2024-10-14 21:02:11 -0400

    animate

--- svgdrawing/script.js
+++ svgdrawing/script.js
@@ -1,8 +1,8 @@
 const dataset = [10,20,30,40,50];
 const svg = d3.select("body")
   .append("svg")
-  .attr("width", 1000)
-  .attr("height", 1000);
+  .attr("width", 600)
+  .attr("height", 600);
 
 svg.selectAll("rect")
   .data(dataset)
@@ -10,7 +10,7 @@ svg.selectAll("rect")
   .append("rect")
   .attr("width", 80)
   .attr("height",50)
-  .attr("transform", (d,i) => `translate(${(i*100)+60},50) rotate(${d})`)
+  .attr("transform", (d,i) => `translate(${(i*100)+60},200) rotate(${d})`)
   .attr("fill", "pink");
 
   svg.selectAll("svg")
@@ -19,7 +19,7 @@ svg.selectAll("rect")
   .append("rect")
   .attr("width", 80)
   .attr("height",50)
-  .attr("transform", (d,i) => `translate(${(i*100)+60},200) rotate(${-d})`)
+  .attr("transform", (d,i) => `translate(${(i*100)+60},300) rotate(${-d})`)
   .attr("fill", "purple");
 
   svg.selectAll("svg")
@@ -28,7 +28,7 @@ svg.selectAll("rect")
   .append("rect")
   .attr("width", 80)
   .attr("height",50)
-  .attr("transform", (d,i) => `translate(${(i*100)+60},280) rotate(${d})`)
+  .attr("transform", (d,i) => `translate(${(i*100)+60},350) rotate(${d})`)
   .attr("fill", "pink");
 
   svg.selectAll("svg")
